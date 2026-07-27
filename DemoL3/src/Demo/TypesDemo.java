f2dda029d2b11ca43baad0710224c8d0887530b5
basgccbrcode <0195659@schoolsnet.act.edu.au>
2026-07-27 11:53:35 +1000

    add

---
+++ DemoL3/src/Demo/TypesDemo.java
@@ -0,0 +1,23 @@
+package Demo;
+
+public class TypesDemo {
+
+	public static void main(String[] args) {
+		int i = 9;
+		System.out.println("Integer: " + i);
+		
+		float f = 3.2f;
+		System.out.println("Float: " + f);
+		
+		double d = 3.2;
+		System.out.println("Double: " + d);
+		
+		char c = 's';
+		System.out.println("Char: " + c);
+		
+		boolean b = true;
+		System.out.println("Bool: " + b);
+
+	}
+
+}
---
+++ Lesson02/src/Q02/InfoPrinter.java
@@ -0,0 +1,10 @@
+package Q02;
+
+public class InfoPrinter {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Bas");
+
+	}
+
+}
