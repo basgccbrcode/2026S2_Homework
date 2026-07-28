ad0c698fba2142961d8c99be082dd991f0256ec3
basgccbrcode <0195659@schoolsnet.act.edu.au>
2026-07-29 09:50:07 +1000

    add lesson4

---
+++ Lesson04/src/demo/TypeAndOperators.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class TypeAndOperators {
+
+	public static void main(String[] args) {
+		// First declare some integers
+		int a = 10, b = 3;
+		double b2 = 3.0;
+		
+		int sum = a + b;
+
+	}
+
+}
