6981f1e47d6abb7eee9ccfe7ec26d5f0eb56c346
basgccbrcode <0195659@schoolsnet.act.edu.au>
2026-07-31 13:11:11 +1000

    add lesson5

---
+++ lesson05/src/demo/Lesson05.java
@@ -0,0 +1,16 @@
+package demo;
+
+import java.util.Scanner;
+
+public class Lesson05 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		
+		// enter your subject
+		
+		System.out.println("enter your score:");
+	}
+
+}
