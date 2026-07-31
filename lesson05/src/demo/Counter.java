c4b8b8148866c2e005d0e1704e1b28d7af40c39e
sebastian carr <0195659@schoolsnet.act.edu.au>
2026-07-31 14:34:15 +1000

    Update print statement in for loop
    Modified statement for the for loop

--- lesson05/src/demo/Counter.java
+++ lesson05/src/demo/Counter.java
@@ -12,7 +12,7 @@ public class Counter {
 
 		System.out.println("for loop");
 		for (int i = 1; i <= 20; i += 2) {
-			System.out.println("number: " + i);
+			System.out.println("Number for count: " + i);
 			if (i == 14) {
 				break;
 			}
