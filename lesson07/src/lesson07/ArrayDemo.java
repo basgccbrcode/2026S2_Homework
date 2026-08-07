6f3c494da287e75bc59abec43f3d72c89287d81c
basgccbrcode <0195659@schoolsnet.act.edu.au>
2026-08-07 14:50:48 +1000

    mod lesson 7

--- lesson07/src/lesson07/ArrayDemo.java
+++ lesson07/src/lesson07/ArrayDemo.java
@@ -1,5 +1,7 @@
 package lesson07;
 
+import java.util.ArrayList;
+
 import com.sun.tools.javac.util.List;
 
 public class ArrayDemo {
@@ -7,11 +9,11 @@ public class ArrayDemo {
 	public static void main(String[] args) {
 		int[] intArray = {2, 4, 6, 8, 10};
 		System.out.println(intArray[2]); // read a value
+	
+		int[] intList = {3, 6, 9, 12, 15}; 
 
+		List<Integer> intList = new ArrayList<>();
 	}
 	
-	int[] intList = {3, 6, 9, 12, 15}; 
-
-	List<Integer> intList = new ArrayList<>();
 	
 }
