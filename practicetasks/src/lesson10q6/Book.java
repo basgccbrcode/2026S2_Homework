84111e71df9a6e37cea43b515293791ec064c044
basgccbrcode <0195659@schoolsnet.act.edu.au>
2026-08-17 12:15:57 +1000

    add practice tasks

--- practicetasks/src/lesson10q6/Book.java
+++ practicetasks/src/lesson10q6/Book.java
@@ -3,8 +3,17 @@ package lesson10q6;
 public class Book {
 	private String title;
 	private String author;
+
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	
 }
 	
+
+	public void display() {
+		System.out.print("%-30s by: %24s\n", this.title, this.author);
+	}
 	
 	
 	public static void main(String[] args) {
@@ -12,6 +21,14 @@ public class Book {
 		Book b2 = new Book("The Hobbit", "Tolkien");
 		Book b3 = new Book("War and Peace", "Leo Tolstoy");
 
+		
+		Book[] books = { book1,
+				 book2,
+				 book3};
+
+for (Book b: books) {
+	b.display();
+}
 	}
 
 }
