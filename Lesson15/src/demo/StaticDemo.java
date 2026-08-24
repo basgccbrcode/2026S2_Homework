e3e6392da69c5fd27f0610a42cf87000ae661847
basgccbrcode <0195659@schoolsnet.act.edu.au>
2026-08-24 12:02:07 +1000

    add lesson15

---
+++ Lesson15/src/demo/StaticDemo.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class StaticDemo {
+
+	public static int counter = 0;
+	
+	public StaticDemo() {
+		StaticDemo.counter += 1;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ practicetasks/src/lesson15q2/MathUtil.java
@@ -0,0 +1,27 @@
+package lesson15q2;
+
+public class MathUtil {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+		
+	public static int subtract(int a, int b) {
+		return a - b;
+	}
+			
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public static void main(String[] args) {
+		int sum = MathUtil.add(8, 0);
+		int diff = MathUtil.subtract(8, 1);
+		int prod = MathUtil.multiply(8, 1);
+		
+		System.out.println("Sum: " + sum);
+		System.out.println("Difference between 8 and 0: " + diff);
+		System.out.println("8 times 1: " + prod);
+	}
+	
+}
